921567a47f3cca81e6f64a3f8b1b4e7ec81a3c87
TakudoTran <tronggga9a@gmail.com>
2022-10-09 20:39:48 +0700

    remake map

---
+++ src/mapData/map empty.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="map empty" tilewidth="8" tileheight="8" tilecount="868" columns="28">
+ <image source="../PacmanMap/arcade empty.png" width="224" height="248"/>
+</tileset>
---
+++ src/mapData/map-full.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="map-full" tilewidth="8" tileheight="8" tilecount="868" columns="28">
+ <image source="../PacmanMap/pacman6.png" width="224" height="248"/>
+</tileset>
