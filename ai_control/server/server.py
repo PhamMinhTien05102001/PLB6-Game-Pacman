7379d6ceffae96e1011d7ac65ed62d68b467018f
PM_Tien <ttiieenn135790@gmail.com>
2022-10-24 21:31:45 +0700

    add api for server ai

--- ai_control/server/server.py
+++ ai_control/server/server.py
@@ -1,18 +1,16 @@
-from re import I
-from flask import Flask, redirect, url_for
-from flask_cors import CORS, cross_origin
+from flask import Flask, redirect
+from flask_cors import cross_origin
 from flask import request
 from flask import render_template, jsonify
 import numpy as np
 import base64
-import requests
 from PIL import Image
 from io import BytesIO
-import io
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import datasets, transforms, models
+from torchvision import transforms, models
+import PIL
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -50,42 +48,66 @@ def pretrain_model(name_model='mobi-v2'):
 
     return pretrain_model
 
-def transform_image(image_bytes):
-    transform = transforms.Compose(
-        [
-            transforms.Resize((240, 240)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5336, 0.5354, 0.5221], [0.2344, 0.2515, 0.2537]),
-        ]
-    )
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    image = image.transpose(method=Image.FLIP_LEFT_RIGHT)
-    return transform(image).unsqueeze(0)
+transformImage = transforms.Compose([
+                                      transforms.Resize((240, 240)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.5345, 0.5550, 0.5419],
+                                                           [0.2360, 0.2502, 0.2615])
+                                    ])
 
-
-def get_prediction(name_model, image):
+def classification(model, image):
+    image = PIL.Image.fromarray(image)
+    image = transformImage(image).float()
+    image = image.unsqueeze(0)
     image.to(device)
-    output = (pretrain_model(name_model))(image)
+    out = model(image)
 
-    _, predictions = torch.max(output.data, 1)
-    prob = F.softmax(output, dim=1)
-    top_p, _ = prob.topk(1, dim=1)
-    return Labels[predictions.item()], top_p.item()
+    _, pre = torch.max(out.data, 1)
+    prob = F.softmax(out, dim=1)
+    top_p, top_class = prob.topk(1, dim = 1)
+    return Labels[pre.item()], top_p.item()
+
 app = Flask(__name__)
 
-@app.route('/<name_model>', methods=['GET', 'POST'])
+@app.route('/test/<name_model>', methods=['GET', 'POST'])
 @cross_origin(origin='*')
 def mainpage(name_model):   
     if request.method == 'POST':
-        image = request.files['imagefile']  # get file
-        image_b64 = base64.b64encode(image.read()).decode('utf-8')
-        # image_save=Image.open(BytesIO(base64.b64decode(image_b64)))
-        # image_save.save("term.png")
-        image = np.frombuffer(base64.b64decode(image_b64), dtype=np.uint8)
-        tensor = transform_image(image)
-        prediction, percent = get_prediction(name_model, tensor)
+        imageFile = request.files['imagefile']  # get file
+        image_b64 = base64.b64encode(imageFile.read()).decode('utf-8')
+        image=Image.open(BytesIO(base64.b64decode(image_b64)))
+        image = np.array(image)
+
+        model = pretrain_model(name_model)
+        prediction, percent = classification(model, image)
+
         return render_template('index.html', name_model=name_model, prediction=prediction, percent=percent) 
     return render_template('index.html', name_model=name_model)
+
+@app.route('/api/<name_model>', methods=['GET', 'POST'])
+@cross_origin(origin='*')
+def apiProcess(name_model):   
+    if request.method == 'POST':
+        try:
+            imageFile = request.files['imageFile']  # get file
+            image_b64 = base64.b64encode(imageFile.read()).decode('utf-8')
+            image=Image.open(BytesIO(base64.b64decode(image_b64)))
+            image = np.array(image)
+
+            model = pretrain_model(name_model)
+            prediction, percent = classification(model, image)
+            data = {'Class Name': prediction, 'Percent': percent*100}
+            print(data)
+            return jsonify(data)
+        except Exception as e:
+            error = "'Error': '" + str(e) + "'"
+            print(error)
+            return jsonify({'Error': str(e)})
+
+@app.route('/')
+@cross_origin(origin='*')
+def init():
+    return redirect('test/mobi-v2')
 
 if __name__ == "__main__":
     app.run(debug=True)
