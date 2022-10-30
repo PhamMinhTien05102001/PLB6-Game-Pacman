b671939d63d283eafcfca166141466e9e7cf9d68
Quang Nguyen <quangnm.work@gmail.com>
2022-10-30 21:11:48 +0700

    feat:auto capture image

--- src/pages/GamePage/components/WebcamGame.tsx
+++ src/pages/GamePage/components/WebcamGame.tsx
@@ -4,9 +4,9 @@ import Webcam from 'react-webcam';
 import styled from 'styled-components';
 import axios from 'axios';
 import './WebCam.css';
-import { Hands } from '@mediapipe/hands';
+// import { Hands } from '@mediapipe/hands';
 // import * as hands from '@mediapipe/hands';
-import { Camera } from '@mediapipe/camera_utils';
+// import { Camera } from '@mediapipe/camera_utils';
 import { Point } from 'react-easy-crop/types';
 import { API_URL } from '../../../constant/index';
 import Cropper from 'react-easy-crop';
@@ -26,18 +26,18 @@ const videoConstraints = {
 // }
 type gestureCount = 'Attack' | 'Bottom' | 'Left' | 'Right' | 'Stop' | 'Top';
 
-const detectHandInBox = (finger: any) => {
-  if (!finger) return false;
-  for (let i = 0; i < finger.length; i++) {
-    if (finger[i].x > 0.3 || finger[i].y > 0.4) {
-      return false;
-    }
-  }
-  return true;
-};
+// const detectHandInBox = (finger: any) => {
+//   if (!finger) return false;
+//   for (let i = 0; i < finger.length; i++) {
+//     if (finger[i].x > 0.3 || finger[i].y > 0.4) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 const ENOUGH_TO_ADD = 2;
-
+const TIME_TO_CAPTURE = 3000;
 const WebcamGame = observer(
   ({
     triggerDirection,
@@ -48,7 +48,7 @@ const WebcamGame = observer(
     const webcamRef = useRef<any>(null);
     const buttonRef = useRef<HTMLButtonElement>(null);
     const [gesture, setGesture] = useState<string>();
-    const [countHandInBox, setCountHandInBox] = useState<number>(0);
+    // const [countHandInBox, setCountHandInBox] = useState<number>(0);
     const gestureCount = {
       Attack: 0,
       Bottom: 0,
@@ -57,75 +57,75 @@ const WebcamGame = observer(
       Stop: 0,
       Top: 0,
     };
-    let camera = null;
+    // let camera = null;
     const [imgSrc, setImgSrc] = useState<any>(null);
 
-    const resetGestureCount = useCallback(() => {
+    const resetGestureCount = () => {
       for (let i in gestureCount) {
         gestureCount[i as gestureCount] = 0;
       }
-    }, [gestureCount]);
+    };
 
-    const detectGesture = useCallback(() => {
+    const detectGesture = () => {
       for (let i in gestureCount) {
         if (gestureCount[i as gestureCount] >= ENOUGH_TO_ADD) {
           resetGestureCount();
           setGesture(i);
         }
       }
-    }, [gestureCount]);
-
-    const onResults = (results: any) => {
-      // console.log(results);
-      setCountHandInBox(val => val + 1);
-
-      // if (countHandInBox === 5) {
-      //   setCountHandInBox(0);
-      //   console.log('Detect hand in box equal 5 and reset');
-      // }
     };
 
+    // const onResults = (results: any) => {
+    //   // console.log(results);
+    //   setCountHandInBox(val => val + 1);
+
+    //   // if (countHandInBox === 5) {
+    //   //   setCountHandInBox(0);
+    //   //   console.log('Detect hand in box equal 5 and reset');
+    //   // }
+    // };
+
     useEffect(() => {
-      if (countHandInBox > 20) {
+      const captureInterval = setInterval(() => {
         buttonRef.current?.click();
-        setCountHandInBox(0);
-      }
-    }, [countHandInBox]);
-
-    useEffect(() => {
-      const hands = new Hands({
-        locateFile: file => {
-          return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-        },
-      });
-      hands.setOptions({
-        maxNumHands: 1,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
-
-      hands.onResults(hand => {
-        // console.log(detectHandInBox(hand.multiHandLandmarks[0]));
-        if (detectHandInBox(hand.multiHandLandmarks[0])) {
-          onResults(hand);
-        }
-      });
-
-      if (
-        typeof webcamRef.current !== 'undefined' &&
-        webcamRef.current !== null
-      ) {
-        camera = new Camera(webcamRef.current.video, {
-          onFrame: async () => {
-            await hands.send({ image: webcamRef.current.video });
-          },
-          width: 800,
-          height: 600,
-        });
-        camera.start();
-      }
+      }, TIME_TO_CAPTURE);
+      return () => clearInterval(captureInterval);
     }, []);
+
+    // useEffect(() => {
+    //   const hands = new Hands({
+    //     locateFile: file => {
+    //       return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    //     },
+    //   });
+    //   hands.setOptions({
+    //     maxNumHands: 1,
+    //     modelComplexity: 1,
+    //     minDetectionConfidence: 0.5,
+    //     minTrackingConfidence: 0.5,
+    //   });
+
+    //   hands.onResults(hand => {
+    //     // console.log(detectHandInBox(hand.multiHandLandmarks[0]));
+    //     if (detectHandInBox(hand.multiHandLandmarks[0])) {
+    //       onResults(hand);
+    //     }
+    //   });
+
+    //   if (
+    //     typeof webcamRef.current !== 'undefined' &&
+    //     webcamRef.current !== null
+    //   ) {
+    //     camera = new Camera(webcamRef.current.video, {
+    //       onFrame: async () => {
+    //         await hands.send({ image: webcamRef.current.video });
+    //       },
+    //       width: 800,
+    //       height: 600,
+    //     });
+    //     camera.start();
+    //   }
+    // }, []);
 
     const capture = useCallback(async () => {
       const imageSrc = webcamRef.current.getScreenshot({});
@@ -139,6 +139,7 @@ const WebcamGame = observer(
 
       const form = new FormData();
       form.append('imageFile', imgCrop);
+      // console.log(imgCrop);
       axios({
         method: 'post',
         url: API_URL,
@@ -150,10 +151,13 @@ const WebcamGame = observer(
       })
         .then(function(response: any) {
           //handle success
-          console.log(response.data);
-          if (response.data['Percent'] >= 90) {
+
+          // console.log(response.data);
+          if (response.data['Percent'] >= 95) {
+            // console.log(response.data['Class Name'], 'Plus one');
             gestureCount[response.data['Class Name'] as gestureCount] += 1;
           }
+
           detectGesture();
           // setGesture(response.data['Class Name']);
         })
